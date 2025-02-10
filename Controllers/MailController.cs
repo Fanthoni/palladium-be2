fb873332c9aa5dfed3b51f5a162b5681c7bc8259
Ferrel <ferrelanthoni@gmail.com>
2025-02-09 22:44:11 -0800

    use gmail

--- Controllers/MailController.cs
+++ Controllers/MailController.cs
@@ -35,7 +35,7 @@ public class MailController : ControllerBase
 
             var smtp = new SmtpClient
             {
-                Host = "smtp.oxcs.bluehost.com",
+                Host = "smtp.gmail.com",
                 Port = 587,
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
